8855830b58fc3e30bd0b50544233d97c55edef38
vudark <vudark@gmail.com>
2017-12-09 00:15:13 -0600

    Added DisplayMessage Activity and Return up button

--- app/src/main/java/com/luccomp163/faisalibrahimnabeel/gradeanalyzer/MainActivity.java
+++ app/src/main/java/com/luccomp163/faisalibrahimnabeel/gradeanalyzer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.luccomp163.faisalibrahimnabeel.gradeanalyzer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
